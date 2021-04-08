0ce336901d6a6c11a6e98f4aac01aed7654339e9
MahdieRad <82083793+MahdieRad@users.noreply.github.com>
2021-04-08 06:58:18 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,9 @@
+A = float(input("Enter 1 Numb: "))
+B = float(input("Enter 2 Num: "))
+C = float(input("Enter 3 Num: "))
+
+if A + B > C and A + C > B and B + C > A:
+    print("You succeeded :) ")
+else:
+    print("Ooooops...Sorry!! Try Again")
+
